e149e8511c85a92c0e8790ff464645b9d01df832
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-05 22:22:02 -0300

    VER MUY BUENO ES OTRO

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,13 +1,36 @@
 
 function mostrar()
 {
-	var base;
+	var numero1;
+	var numero2;
+	var numero3;
+	var suma;
+	var promedio;
+	var resta;
+
+	numero1=prompt("Ingrese valor 1");
+	numero1=parseInt(numero1);
+	numero2=prompt("Ingrese valor 2");
+	numero2=parseInt(numero2);
+	numero3=prompt("Ingrese valor 3");
+	numero3=parseInt("numero3");
+
+	suma=numero1+numero2+numero3;
+	promedio=(numero1+numero2+numero3)/3;
+	resta=numero1-numero2-numero3;
+
+	alert("La suma" + suma + // etc con las otras   ver bien!! el promedio y la resta es igual a:");
+
+
+
+
+}
+
+/*var base;
 	var altura;
 	var perimetro;
 	var superficie;
 
 	base=prompt("Ingresar la base del triangulo equilatero");
-	altura=prompt("Ingresar la altura del triangulo equilatero");
-	
-
-}
+	altura=prompt("Ingresar la altura del triangulo equilatero");*/
+//Se ingresan 3 numeros informar en un solo alert la suma, el promedio, y la resta de los numeros tal cual como fueron ingresados.
