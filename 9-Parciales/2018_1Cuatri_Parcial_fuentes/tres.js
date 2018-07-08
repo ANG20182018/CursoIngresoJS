57fe2d27d97798380aec690c7492fa463e7113cc
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-08 10:18:32 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+	var precio;
+	var pDescuento;
+	var descuentoenprecio;
+	var elPrecioFinal;
+
+	precio=prompt("Ingrese el precio del producto: ");
+	precio=parseInt(precio); 
+	pDescuento=prompt("Ingrese el porcentaje de descuento: ");
+	pDescuento=parseInt(pDescuento);
+
+	descuentoenprecio= (precio * pDescuento ) / 100;
+	elPrecioFinal= precio - descuentoenprecio; 
+
+	document.getElementById('elPrecioFinal').value=elPrecioFinal;
+
 
 }
