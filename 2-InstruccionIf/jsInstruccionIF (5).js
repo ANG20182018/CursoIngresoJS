6a1248bd38083705b91a16f9657053fe63c8040f
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-12 19:44:39 -0300

    EJ EXPLICACION CONECTOR LOGICO O

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad <13 || edad>17)      // EL O ES ALT GR +1 OJO...SINO ES COMENTAR SUP IMP
+	{
+		alert("La persona no es adolescente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
