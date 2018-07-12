ce214f1c010b38ac61a55663069456f8be0f2b49
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-12 19:38:00 -0300

    IMP UTILIZACION DE IF Y INTERSECCION
    + RESUMEN

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,23 +2,19 @@ function mostrar()
 {
 	var edad;
 
-	edad=document.getElementById('edad').value;
+	edad=document.getElementById('edad').value; //ME HABIA EQUIVOCADO EN LOS SIGNOS < Y > POR ESO NO ANDABA...
 
-	if(edad>=18)
+	if(edad >=13 && edad <= 18)  //ATENCION SON BINARIOS NECESITAN LA EDAD DE AMBOS LADOS!!!
 	{
-		alert("La persona es un adulto");
-	}
-	else
-	{
-	if(edad>=13) //no puede haber un corchete antes del if..mirar bien.
-	{	
-		alert("La persona es un adolescente");
-	}	
-	else
-	{
-		alert ("La persona es un niño");	
-	}
-	}
-	
+		alert("Es un adolescente");
+	}	 
 
 
+
+}//FIN DE LA FUNCIÓN
+
+/*  a>13   y a<17  te pide la interseccion de los 2 conjuntos, usar CONECTORES LOGICOS, Y U O ATENCION, 
+	CUANDO PIDE LA UNION DE AMBOS ES EL O ...SUPER IMPPPPP
+	UN O SE USA CUANDO PIDE LA UNION DE 2 SEGMENTOS SEPARADOS EJEMPLO SI TE PIDE LA EDAD HASTA 13 Y DE 17 EN ADELANTE
+
+	EN UNO SE USA Y Y EN EL OTRO O    */
