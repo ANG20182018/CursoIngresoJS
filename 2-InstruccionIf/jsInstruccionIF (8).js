01fe987d71263af5d3ad8b128597f74331396c70
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-12 20:33:06 -0300

    VER ESTE TUVE VARIAS DIF EXPLICA NO HACER NADA
    CUANDO DICE NO HACER NADA, NO HAY QUE TIPEAR NADA EN EL CODIGO

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estadoCivil;
+
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+	//if (edad<18 && estadoCivil != "Soltero") // NO SE PUEDE PONER ESTO ASI, CUANDO DICE NO HACER NADA, NO HAY QUE ESCRIBIR NADA
+	//me tiraba error por que tomaba la linea del else como condicion...
+		
+		if (estadoCivil == "Soltero" && edad>=18)
+		{
+			alert("Es soltero y no es menor");
+		}
+	
+
+
 
 	
 
 
 }//FIN DE LA FUNCIÓN
+
+/*EL NO HACER NADA SIGNIFICA NO ESCRIBIR NI UNA SOLA LINEA DE CODIGO OJO!!!*/
