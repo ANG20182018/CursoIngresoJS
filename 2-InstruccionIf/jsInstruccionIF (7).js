b98cd53d81b00c0837cd6a4a6845420b90ac5bee
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-12 20:14:18 -0300

    EJ 7 CON CUADROS DE TEXTOS CON MULT OPCIONES
    VER EN LA PARTE DE SELECCION DE VARIABLES

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estadoCivil;
+
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+	if(edad<18 && estadoCivil != "Soltero") //ACA SE PONE ENTRE COMILLAS PARA QUE LO LEA, SINO NO LO COMPARABA, ERA UN STRING...
+	{
+		alert("Es muy pequeño para no ser soltero");
+	}	
 
 	
 
