56c5307cd74e01d2614354a8e7d55108fa7d8ac4
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-12 21:21:52 -0300

    EXPLICACION DE FORMULA RANDOM X EL PROFE VER!!

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -9,3 +9,26 @@ function mostrar()
 	
 
 }//FIN DE LA FUNCIÓN
+/*
+ SUPER IMPORTANTE
+ EXPLICACION RANDOM
+
+ EN LA CONSOLA PUSO Math, ESO DESPLEGO LA LIBRERIA, SE FIJO EN LAS CARPETAS, DESPUES DE LAS CONSTANTES, 
+ CUYO VALOR ES SIEMPRE EL MISMO, ESTAN TODAS LAS FORMULAS MATEMATICAS QUE EJECUTA JAVAS
+ AHI BUSCO LA FUNCION RANDOM QUE ES:
+
+ Math.floor(Math.random()* (max-min)) + min
+
+ .floor es base por que redondean los numeros para abajo.
+  
+  tiene que redondear para abajo por que si al random no le asignas nada un valor toma valores
+  automaticamente entre 0 y 1 pero no llega al 1.
+
+  el min y max son los valores entre los cuales se quiere realizar la operacion ej entre 10 y 20
+  TENER EN CUENTA QUE SIEMPRE SE LE PONE UN NUMERO MAS AL MAXIMO O SEA SI ES HASTA 20 SE PONE 21, SUPER IMPPPP!!!!
+  POR LA EXPLICACION ANTERIOR Q CUENTA ENTRE 0 Y 1...
+
+
+
+
+*/
