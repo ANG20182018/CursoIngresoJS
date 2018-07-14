9f997bc122c15f198e010495ca9ef3f7fcb5ee43
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-14 12:50:45 -0300

    OJO ACA ESTA LA EXPLICACION BIEN HECHO
    MIRAR, YO LO ESTABA HACIENDO MAL!

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -13,35 +13,87 @@ function CalcularPrecio ()
  	var precio;
  	var cantidad;
  	var marcas;
- 	var precioDescuento;
+ 	//var precioDescuento; va o no?? /LA SOLUCION ES ANIDAR LOS IF Y LOS ELSE!
 
- 	precio=35;
+ 	precio=35; // siempre que es un numero se recomienda parsearlo!
  	precio=parseInt(precio);
   	cantidad=document.getElementById('Cantidad').value;
  	cantidad=parseInt(cantidad);
  	marcas=document.getElementById('Marca').value;
 
-
- 	if(cantidad>=6)
+ 	
+ 	if(cantidad >= 6) 
  	{
- 		document.getElementById('precioDescuento').value= (precio * cantidad) * 50/100 ;
+ 		document.getElementById('precioDescuento').value= (precio * cantidad) * 50 /100;
  	}
- 	else if(cantidad>=5 && marcas =="ArgentinaLuz")
+
+ 					
+ 	else if(cantidad == 5 && marcas =="ArgentinaLuz") //el error esta en que ejecuta el if 1, despues pasa al if 2 y como es false ejecuta el else, en los errores aparecen todas las ejecuciones del else...
  	{
- 		document.getElementById('precioDescuento').value= (precio * cantidad) * 60/100; // warda con los porcentajes!
+ 		document.getElementById('precioDescuento').value= (precio * cantidad) * 60 /100; // warda con los porcentajes!
  	}
  	else
+ 			{
+ 				document.getElementById('precioDescuento').value= (precio * cantidad) * 70/100; // aca hay un desc del 30!!
+ 			}
+	
+ 	
+ 	if(cantidad==4 && (marcas == "ArgentinaLuz" || marcas=="FelipeLamparas")) //me faltaba cerrar el parentesis! y repetir marcas, sino no lo leia!!ojo super imppp!!
  	{
- 		document.getElementById('precioDescuento').value= (precio * cantidad) * 80/100; // aca hay un desc del 20!!
+ 		document.getElementById('precioDescuento').value= (precio * cantidad) * 75/100;
  	}
- 	if (cantidad==4 && (marcas == "ArgentinaLuz" || marcas=="FelipeLamparas")) //me faltaba cerrar el parentesis! y repetir marcas, sino no lo leia!!ojo super imppp!!
+ 	else
+ 			{
+ 				document.getElementById('precioDescuento').value= (precio * cantidad) * 80/100;
+ 			}
+
+
+ 	if(cantidad==3 && marcas== "ArgentinaLuz")
  	{
- 		document.getElementById('precioDescuento').value= (precio * cantidad) * 80/100;
+ 		document.getElementById('precioDescuento').value= (precio * cantidad) * 85/100;
  	}
- 	}
+ 	
+ 	else if (cantidad==3 && marcas=="FelipeLamparas")
+ 			{
+ 				document.getElementById('precioDescuento').value= (precio * cantidad) * 90/100;
+ 			}
+ 	
+ 	else
+ 			{
+ 				document.getElementById('precioDescuento').value= (precio * cantidad) * 95/100;
+ 			}
+ 	
+
+ 	 }
  	
 
 
+    /*SUPER IMPORTANTE ! UNO SOLO ES PARA NEGARLO, PARA NEGAR LA FRASE   EJ: SE PONE ENTRE UNA ECUACION ANTES DE TODO, Y SI LA ECUACION DA TRUE, LO CONVIERTE EN FALSE
+
+		if ((document.getElementById('precioDescuento').value) > 120 )
+
+ 			
+ 				document.getElementById('precioDescuento').value= (document.getElementById('precioDescuento').value) * 110/100;
+ 				alert(" Se agrego 10% mas de IIBB");
 
 
-}
+// los if se separan en condiciones, pero si la condicion incluye lo mismo se hace if else, pero no hay que hacer if else sino comparten condicion!
+//si te equivocas y pones ; aca se ejecuta la parte de abajo aisladamente...
+    
+	explicacion en clase de apoyo:
+
+	EL DEFINIO VAR PRECIO BRUTO=(CANTIDAD DE LAMPARAS X 35) OJOOOO SUPPPPP IMP
+
+	ESTABLECIO: TOTAL CON DESCUENTO = TOTAL BRUTO - DESCUENTO;
+
+	Y MUESTRA UNICAMENTE AL FINAL UN SOLO GET ELEMENT= TOTAL CON DESCUENTO  SUP IMP!
+
+	IF CONDICION=
+
+	DESCUENTO= TOTAL BRUTO * 0.5 (PARA HACER EL 50% DE DESCUENTO) (0.4 X PRECIO BRUTO PARA SACAR EL 40% DE DESCUENTO!)
+	(EL VALOR DE DESCUENTO SE LO DA DESPUES DE LA EJECUCUIN DEL IF)
+
+
+    */
+
+
