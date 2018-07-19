4fcf1bfb24b8d76882b61fe1e4308f6ff2e88b3c
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-19 10:21:33 -0300

    PPT 2 PERO NO MUESTRA EL RESULTADO FINAL VER!!

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -3,29 +3,74 @@ var ContadorDeEmpates=0;
 var ContadorDeGanadas=0;
 var ContadorDePerdidas=0;
 
+
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
-
+	eleccionMaquina=Math.floor(Math.random() * 3 + 1);
+	alert(eleccionMaquina);
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	if (eleccionMaquina==1)
+	{
+		alert("Piedra y Piedra, empato...");
+		ContadorDeEmpates=ContadorDeEmpates+1;
+	}
+	else if (eleccionMaquina==2)
+	{
+		alert("Piedra y papel, perdio...");
+		ContadorDePerdidas=ContadorDePerdidas+1;
+	}
+	else
+	{
+		alert("Piedra y tijera, gano...");
+		ContadorDeGanadas=ContadorDeGanadas+1;
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	if (eleccionMaquina==2)
+	{
+		alert("Papel y papel, empato...");
+		ContadorDeEmpates=ContadorDeEmpates+1;
+	}
+	else if (eleccionMaquina ==3)
+	{
+		alert("Papel y tijera, perdio...");
+		ContadorDePerdidas=ContadorDePerdidas+1;
+	}
+	else
+	{
+		alert("Papel y piedra, gano...");
+		ContadorDeGanadas=ContadorDeGanadas+1;
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
+	if (eleccionMaquina==3)
+	{
+		alert("Tijera y Tijera, empato...");
+		ContadorDeEmpates=ContadorDeEmpates+1;
+	}
+	else if (eleccionMaquina==1)
+	{
+		alert("Tijera y piedra, perdio...");
+		ContadorDePerdidas=ContadorDePerdidas+1;
+	}
+	else
+	{
+		alert("Tijera y papel, gano...");
+		ContadorDeGanadas=ContadorDeGanadas+1;
+	}
 	
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
+	alert ("Usted gano: " + ContadorDeGanadas + "veces, " + "Perdio: " + ContadorDePerdidas + "veces" + "  y empato..." + ContadorDeEmpates + "..veces");
 
 }
