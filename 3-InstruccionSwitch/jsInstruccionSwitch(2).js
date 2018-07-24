46bb6dccdc024afc12df16cceacdea2856e43ac5
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-23 21:38:12 -0300

    switch  corregido ok ! tomar de aca para estudiar!

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,51 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño;
 
-alert (mesDelAño);
+mesDelAño=document.getElementById('mes').value;
+
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	
+		alert ("Falta para el invierno");
+		break;
+	
+
+	case "Julio":
+	case "Agosto":
+	
+		alert(" Es invierno, abrigate");
+		break;
+	
+	default: // PARA VER SI ENTENDI OK, ACA SE USA DEFAULT COMO UN ELSE, NO HACE FALTA DEFINIR TODO...
+				//MI OTRO ERROR FUE PONER {}, EL SWITCH NO LOS NECESITA, SOLO ESCRIBIR ABAJO DE LOS 2 PUNTOS Y ESTA OK!
+
+		alert("Ya pasamos el invierno, ahora el calor");
+		break;
+
+
+	/*case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	{
+		alert("Ya pasamos el invierno, ahora el calor! ");
+		break;
+
+	}*/
+
+
+
+
+
+}
 
 
 
