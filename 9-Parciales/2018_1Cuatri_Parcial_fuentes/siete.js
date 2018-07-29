ed06042558931728f8f58c87c6cf7ceb3d09f7fe
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-29 18:38:16 -0300

    PARA PREGUNTAR NO SE HACERLO!!!
    DUDAS AL PRINCIPIO...

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,166 @@
 function mostrar()
+
 {
+	var notas;
+	var sexo;
+	var contador =0;
+	var acumulador = 0;
+	var maximo;
+	var minimo;
+
+//NO LO PUDE HACER: 1) NO SE SI SE PUEDE USAR WHILE DENTRO DE WHILE Y SI SE PUEDE VALIDAR USANDO IF...
+// 2) NO SE COMO HACER PARA UNIFICAR DATOS DE 2 TIPOS DIFERENTES, SEXO Y NOTA, GUARDARLOS POR SEPARADO Y MOSTRARLOS...
+
+
+	while (contador < 5 ) // ES MEJOR HACERLO WILE E IF, NO SE SI SE PUEDEN HACER WHILE WHILE...
+	{	
+		contador ++;
+		notas=prompt( "Ingrese notas: ");
+		notas=parseInt(notas);
+		sexo= prompt ("Ingrese su sexo: ");
+
+		if ((notas >= 0 && notas <= 10) && (sexo == "m" || sexo == "f"))
+		{
+			acumulador= acumulador + notas;
+		}
+		else
+		{
+			alert("No ingreso una nota  o un sexo valido...");
+			contador= contador -1;
+		}
+		
+		
+		if (contador ==1)
+		{
+			notas=maximo;
+			notas=minimo;
+		}
+		else if (nota > minimo)
+		{
+			nota=maximo;
+		}
+		else if (nota < maximo)
+		{
+			nota =minimo;
+		}
+			
+
+
+	}
+
+	alert( " El promedio de las notas es: " + (acumulador / 5 ));
+	alert (" La nota mas baja..." + minimo);
+
+
+
+
+	
+	/*if (notas >= 0 && notas <= 10)
+		{
+			notas=prompt( "Ingrese notas: ");
+			notas=parseInt(notas);
+		}
+		else
+		{
+			alert ("La nota ingresada no es valida, intente nuevamente...");
+			notas=prompt( "Ingrese notas: ");
+			notas=parseInt(notas);
+		}
+		
+		//sexo= prompt ("Ingrese su sexo: ");/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*switch(notas)
+	{
+		case 0:
+		{
+			notas=0;
+			break;
+		}
+		case 1:
+		{
+			notas=1;
+			break;
+		}
+		case 2:
+		{
+			notas=2;
+			break;
+		}
+		case 3:
+		{
+			notas=3;
+			break;
+		}
+		case 4:
+		{
+			notas=4;
+			break;
+		}
+		case 5:
+		{
+			notas=5;
+			break;
+		}
+		case 6:
+		{
+			notas=6;
+			break;
+		}
+		case 7:
+		{
+			notas=7;
+			break;
+		}
+		case 8:
+		{
+			notas=8;
+			break;
+		}
+		case 9:
+		{
+			notas=9;
+			break;
+		}
+		case 10:
+		{
+			notas=10;
+			break;
+		}
+		
+		default:
+		{
+			alert("La nota ingresada no es valida");
+			break;
+		}
+
+
+	}
+*/
+
+
 
 }
