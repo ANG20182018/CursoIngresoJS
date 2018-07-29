1684f25c9a34d1be83178d7ece8df8f0e7ff292b
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-07-29 19:19:24 -0300

    8, NO SE HACER LA ULTIMA PARTE...VER EN CLASE!
    VER TAMBIÉN SI ESTA VALIDADO OK!

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+	var letra;
+	var numero=0;
+	var respuesta = "si"; //VER LA ULTIMA PARTE COMO ARTICULAR 2 TIPOS DISTINTOS DE DATOS Y VER SI ASI ESTA VALIDADO OK!
+	var contadorPares=0;
+	var contadorInpares=0;
+	var contadorCero = 0;
+
+	while (respuesta != "no")
+	{
+		letra= prompt ("Ingrese letra..");
+		
+		
+		if (numero > -100 && numero < 100)
+		{
+			numero= prompt("Ingrese numero..:");
+			numero= parseInt(numero);
+		}
+
+		if (numero %2 ==0)
+		{
+			contadorPares = contadorPares + 1; 
+		}
+		else
+		{
+			contadorInpares=contadorInpares + 1;
+		}
+
+		if (numero ==0)
+		{
+			contadorCero= contadorCero + 1 ;
+		}
+
+
+		respuesta= prompt("Desea continuar ingresando los numeros? ");
+	}
+
+
+
+
+
+
+
+
+
 
 }
+
