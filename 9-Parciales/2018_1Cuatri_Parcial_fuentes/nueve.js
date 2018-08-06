c0aaea335b98d9ac96400cd17c440cf40275aa4d
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-08-06 17:51:10 -0300

    8  Y 9 HECHOS OK EN CASA!

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,86 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperaturaAlmacenamiento;
+	var respuesta = "si";
+	var contador = 0;
+	var contadorTemperaturasPares = 0;
+	var pesoMaximo;
+	var pesoMinimo;
+	var marcaMasPesado;
+	var contadorConservarBajoCero = 0;
+	var acumuladorPesoTodos = 0; 
+
+
+	while (respuesta != "no") // ojo aca, va el opuesto del de arriba...
+	{
+		contador ++;
+
+		marca=prompt("Ingrese la marca del producto: ");
+
+		peso = prompt ("Ingrese el peso ");
+		peso = parseInt (peso);
+
+		while ( isNaN (peso) || peso < 1 || peso > 100 ) // siempre que haya numeros, isnan para que no ingresen letras...
+		{
+			peso = prompt ("Ingrese el peso ");
+			peso = parseInt (peso);
+		}
+
+		temperaturaAlmacenamiento= prompt("Ingrese la temperatura de Almacenamiento...: ");
+		temperaturaAlmacenamiento = parseInt (temperaturaAlmacenamiento);
+
+		while ( isNaN (temperaturaAlmacenamiento) || temperaturaAlmacenamiento < -30 || temperaturaAlmacenamiento > 30 )
+		{
+			temperaturaAlmacenamiento= prompt("Ingrese la temperatura de Almacenamiento...: ");
+			temperaturaAlmacenamiento= parseInt (temperaturaAlmacenamiento);
+
+		}
+
+		if (temperaturaAlmacenamiento %2 == 0)
+		{
+			contadorTemperaturasPares ++;
+		}
+
+		if (contador == 1)
+		{
+			pesoMinimo = peso;
+			pesoMaximo = peso; 
+			marcaMasPesado= marca; // si haces solo una vez se queda aca... sino lo pones no anda!
+		}
+		else if ( peso > pesoMaximo ) //ASI S ESACAN OK MAX Y MINIMOS, O SEA PESO NUEVO ES MAYOR AL PESO MAXIMO ANTERIOR, ES EL NUEVO PESO MAXIMO...
+			pesoMaximo = peso;
+			marcaMasPesado= marca;
+		}
+		else if ( peso < pesoMinimo) // SI EL PESO NUEVO ES MENOR AL PRECIO MINIMO ANTERIOR, ES EL NUEVO PRECIO MINIMO!
+		{
+			pesoMinimo = peso;
+
+		}
+
+		if (temperaturaAlmacenamiento < 0 )
+		{
+			contadorConservarBajoCero ++;
+		}
+
+		acumuladorPesoTodos= acumuladorPesoTodos + peso; // esto tiene q estar afuera del bucle, sino se reinicia siempre...
+
+
+		respuesta = prompt (" Desea seguir ingresando datos ? "); 
+	
+	}
+
+
+	document.write(" <br> Cantidad de temperatura de almacenamiento Pares: " + contadorTemperaturasPares );
+	document.write(" <br> Marca mas pesado: " + marcaMasPesado); 
+	document.write(" <br> Cantidad de productos que se conservan bajo 0: " + contadorConservarBajoCero);
+	document.write(" <br> El peso promedio de todos los productos es: " + (acumuladorPesoTodos / contador) );
+	document.write(" <br> Peso Maximo: " + pesoMaximo + "Peso minimo: " + pesoMinimo );
+
+
+
+
+	
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,42 +1,103 @@
 function mostrar()
 {
 	var letra;
-	var numero=0;
-	var respuesta = "si"; //VER LA ULTIMA PARTE COMO ARTICULAR 2 TIPOS DISTINTOS DE DATOS Y VER SI ASI ESTA VALIDADO OK!
-	var contadorPares=0;
-	var contadorInpares=0;
+	var numero;
+	var respuesta = "si";
+	var contador=0; 
+	var contadorPares =0;
+	var contadorImpares = 0;
 	var contadorCero = 0;
+	var contadorPositivos =0;
+	var acumuladorPositivos = 0;
+	var acumuladorNegativos = 0;
+	var maximo;
+	var minimo;
+	var letraMaximo;
+	var letraMinimo;
+
 
 	while (respuesta != "no")
 	{
-		letra= prompt ("Ingrese letra..");
-		
-		
-		if (numero > -100 && numero < 100)
-		{
-			numero= prompt("Ingrese numero..:");
-			numero= parseInt(numero);
-		}
+		contador ++;
+		letra=prompt("Ingrese la Letra: ");
 
-		if (numero %2 ==0)
+		/*while ( letra != isNaN (numero) ) // OjO VER VALIDACION DE LETRAS!
 		{
-			contadorPares = contadorPares + 1; 
+			letra=prompt("Ingrese la Letra: ");
+		}*/
+
+		numero=prompt("Ingrese numero: ");
+		numero=parseInt(numero);
+
+		while (isNaN(numero) || numero < -100 || numero > 100)
+		{
+			numero=prompt("Ingrese numero: ");
+			numero=parseInt(numero);
+
+		}
+		
+		if (numero %2==0)
+		{
+			contadorPares++;
 		}
 		else
 		{
-			contadorInpares=contadorInpares + 1;
+			contadorImpares ++;
 		}
 
-		if (numero ==0)
+		
+		if (numero == 0)
 		{
-			contadorCero= contadorCero + 1 ;
+			contadorCero ++;
+		}
+		else if (numero > 0 )
+		{
+			contadorPositivos ++;
+			acumuladorPositivos = acumuladorPositivos + numero; 
+		}
+		else
+		{
+			acumuladorNegativos= acumuladorNegativos + numero;
+		}
+
+		
+		if (contador ==1)
+		{
+			maximo = numero;
+			minimo = numero;
+			letraMaximo= letra;
+			letraMinimo = letra;
+		}
+		else if ( maximo > numero) //OJO ASI SE HACE OK!
+		{
+			minimo = numero;
+			letraMinimo= letra;
+		}
+		else if ( minimo < numero) 
+		{
+			maximo = numero;
+			letraMaximo = letra;
 		}
 
 
-		respuesta= prompt("Desea continuar ingresando los numeros? ");
+		respuesta= prompt ("Desea seguir ingresando numeros?: "); 
+
 	}
+	
+	document.write (" <br> Cantidad de numeros Pares: " + contadorPares ); 
+	document.write (" <br> Cantidad de numeros imPares: " + contadorImpares );
+	document.write (" <br> Cantidad de ceros: " + contadorCero );
+	document.write (" <br> Cantidad de positivos: " + contadorPositivos);
+	document.write (" <br> Promedio de positivos: " + acumuladorPositivos / contadorPositivos);
+	document.write (" <br> Suma de negativos: " + acumuladorNegativos);
+	document.write (" <br> Numero del maximo: " + maximo + "Letra del Maximo: " + letraMaximo);
+	document.write (" <br> Numero del minimo: " + minimo + "Letra del minimo: " + letraMinimo);
 
 
+
+
+
+	
 
 
 
